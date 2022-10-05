e1fd59d9e27666f3d5f1b58c83e86cc2bc210fcb
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2022-10-05 11:35:12 +0200

    Fixed missing double-quotes needed for paths with spaces. (#3)
    * Fixed missing double-quotes needed for paths with spaces.

--- coding-style.sh
+++ coding-style.sh
@@ -17,14 +17,14 @@ then
     REPORTS_DIR=$(readlink -f "$2")
     EXPORT_FILE="$REPORTS_DIR"/coding-style-reports.log
     ### delete existing report file
-    rm -f $EXPORT_FILE
+    rm -f "$EXPORT_FILE"
 
     ### Pull new version of docker image and clean olds
     sudo docker pull ghcr.io/epitech/coding-style-checker:latest && sudo docker image prune -f
 
     ### generate reports
     sudo docker run --rm -i -v "$DELIVERY_DIR":"/mnt/delivery" -v "$REPORTS_DIR":"/mnt/reports" ghcr.io/epitech/coding-style-checker:latest "/mnt/delivery" "/mnt/reports"
-    [[ -f $EXPORT_FILE ]] && echo "$(wc -l < $EXPORT_FILE) coding style error(s) reported in $EXPORT_FILE, $(grep -c ": MAJOR:" $EXPORT_FILE) major, $(grep -c ": MINOR:" $EXPORT_FILE) minor, $(grep -c ": INFO:" $EXPORT_FILE) info"
+    [[ -f "$EXPORT_FILE" ]] && echo "$(wc -l < "$EXPORT_FILE") coding style error(s) reported in "$EXPORT_FILE", $(grep -c ": MAJOR:" "$EXPORT_FILE") major, $(grep -c ": MINOR:" "$EXPORT_FILE") minor, $(grep -c ": INFO:" "$EXPORT_FILE") info"
 else
     cat_readme
 fi
