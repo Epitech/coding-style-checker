50e3c88833800c60d434bece38953da28a01be3d
COLAMAroro <github@rondier.io>
2023-03-09 15:40:08 +0100

    Add cross-platform support

--- flake.nix
+++ flake.nix
@@ -2,84 +2,84 @@
   description = "Print EPITECH's coding style compliance report";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    vera.url = "github:Epitech/banana-vera";
-    vera.flake = false;
+    vera-fork.url = "github:Epitech/banana-vera";
+    vera-fork.flake = false;
     ruleset.url = "git+ssh://git@github.com/Epitech/banana-coding-style-checker.git";
     ruleset.flake = false;
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, vera, ruleset, ... }@inputs:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      packages.x86_64-linux.vera = pkgs.stdenv.mkDerivation {
-        pname = "vera++";
-        version = "1.3.0";
+  outputs = { self, nixpkgs, vera-fork, ruleset, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+          packages = flake-utils.lib.flattenTree rec {
+            vera = pkgs.stdenv.mkDerivation {
+              pname = "vera++";
+              version = "1.3.0";
 
-        src = vera;
+              src = vera-fork;
 
-        nativeBuildInputs = [ pkgs.cmake ];
-        buildInputs = [
-          pkgs.python3
-          (pkgs.boost.override { enablePython = true; python = pkgs.python3; })
-          pkgs.tcl
-        ];
+              nativeBuildInputs = [ pkgs.cmake ];
+              buildInputs = [
+                pkgs.python3
+                (pkgs.boost.override { enablePython = true; python = pkgs.python3; })
+                pkgs.tcl
+              ];
 
-        cmakeFlags = [
-          "-DVERA_LUA=OFF"
-          "-DVERA_USE_SYSTEM_BOOST=ON"
-          "-DPANDOC=OFF"
-        ];
-      };
-      packages.x86_64-linux.report = pkgs.writeShellScriptBin "cs" ''
-        start_time=$(date +%s%3N)
+              cmakeFlags = [
+                "-DVERA_LUA=OFF"
+                "-DVERA_USE_SYSTEM_BOOST=ON"
+                "-DPANDOC=OFF"
+              ];
+            };
+            report = (pkgs.writeShellScriptBin "cs" ''
+              start_time=$(date +%s%3N)
 
-        if [ -z "$1" ]; then
-            project_dir=$(pwd)
-        else
-            project_dir="$1"
-        fi
+              if [ -z "$1" ]; then
+                  project_dir=$(pwd)
+              else
+                  project_dir="$1"
+              fi
 
-        echo "Running norm in $project_dir"
-        files=$(find "$project_dir"  \
-            -type f                  \
-            -not -path "*/.git/*"    \
-            -not -path "*/.idea/*"   \
-            -not -path "*/.vscode/*" \
-            -not -path "bonus/*"     \
-            -not -path "tests/*"     \
-            -not -path "/*build/*"   \
-        )
+              echo "Running norm in $project_dir"
+              files=$(find "$project_dir"  \
+                  -type f                  \
+                  -not -path "*/.git/*"    \
+                  -not -path "*/.idea/*"   \
+                  -not -path "*/.vscode/*" \
+                  -not -path "bonus/*"     \
+                  -not -path "tests/*"     \
+                  -not -path "/*build/*"   \
+              )
 
-        echo "Checking $(echo $files | wc -w) files"
-        # shellcheck disable=SC2046
-        output=$(${self.packages.x86_64-linux.vera}/bin/vera++ \
-            --profile epitech                                  \
-            --root ${ruleset}/vera                             \
-            -d $(echo "$files" | tr '\n' ' ')                  \
-        )
+              echo "Checking $(echo $files | wc -w) files"
+              # shellcheck disable=SC2046
+              output=$(${packages.vera}/bin/vera++  \
+                  --profile epitech                 \
+                  --root ${ruleset}/vera            \
+                  -d $(echo "$files" | tr '\n' ' ') \
+              )
 
-        if [ -z "$output" ]; then
-            echo "No issue found."
-        else
-            escaped_path=$(echo $project_dir | sed 's/\//\\\//g')
-            echo "$output" | sed "s/$escaped_path\///g"
-            echo "Found $(echo "$output" | grep -c "$") issues"
-        fi
-        end_time=$(date +%s%3N)
-        echo "Ran in $((end_time - start_time))ms"
-      '';
-      packages.x86_64-linux.default = self.packages.x86_64-linux.report;
+              if [ -z "$output" ]; then
+                  echo "No issue found."
+              else
+                  escaped_path=$(echo $project_dir | sed 's/\//\\\//g')
+                  echo "$output" | sed "s/$escaped_path\///g"
+                  echo "Found $(echo "$output" | grep -c "$") issues"
+              fi
+              end_time=$(date +%s%3N)
+              echo "Ran in $((end_time - start_time))ms"
+            '');
+            default = report;
+          };
 
-      apps.x86_64-linux.vera = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.vera}/bin/vera++";
-      };
-      apps.x86_64-linux.report = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.report}/bin/cs";
-      };
-      apps.x86_64-linux.default = self.apps.x86_64-linux.report;
-    };
+          apps.vera.type = "app";
+          apps.vera.program = "${packages.vera}/bin/vera++";
+
+          apps.report.type = "app";
+          apps.report.program = "${packages.report}/bin/cs";
+          apps.default = apps.report;
+        });
 }
 
